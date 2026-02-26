2e707494c46315d6335b9281e66b8201d39e2472
2502150-hash <2502150@students.au.edu.pk>
2026-02-26 09:42:42 +0500

    Add files via upload
    have done all lab tasks and uploaded them

---
+++ task7main.js
@@ -0,0 +1,9 @@
+// main.js
+
+import { add, subtract } from './math-utils.js';
+
+const sum = add(10, 5);
+const difference = subtract(20, 7);
+
+console.log("10 + 5 =", sum);
+console.log("20 - 7 =", difference);
